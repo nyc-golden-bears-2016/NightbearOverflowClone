fb4ead5a081eafbd5e732eab4e650aeaa516d07d
ericatalyst <erica825978@gmail.com>
2016-07-20 17:14:04 -0400

    All validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,13 @@
 class Tag < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :questions
-  
+
   validates :subject, presence: true
 
     # def tag_popularity_today
     #   #assumes time being tested is a UTC ruby DateTime object
     #   tagged = []
-    #   tags = QuestionTag.where(tag: self).(created_at: :desc)
+    #   tags = QuestionTag.where(tag: self).order_by(created_at: :desc)
     #   slice at one day ago
     #   tags.each do |tag|
     #     if tag.created_at > DateTime.now.to_a[3..5] && < DateTime.now.to_a[3..5]
