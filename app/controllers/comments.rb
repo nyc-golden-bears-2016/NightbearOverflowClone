cfcca3c4f96c5533580e341c71c2b630623e4168
Aleksander Turberville <aleksville@live.com>
2016-07-21 11:32:16 -0400

    fixed adding comments to questions and answers

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,35 +1,31 @@
-get '/comments/question/:question_id/new' do
-  @question = Question.find(params[:question_id])
+get '/questions/:id/comments/new' do
+  @question = Question.find(params[:id])
   erb :'/comments/new'
 end
 
-get '/comments/answer/:answer_id/new' do
-  @answer = Answer.find(params[:answer_id])
+get '/answers/:id/comments/new' do
+  @answer = Answer.find(params[:id])
   erb :'/comments/new'
 end
 
-post '/comments/question' do
-    @question = Question.find(params[:question_id])
-    @comment = @question.comments.new(content:params[:content],user_id:current_user.id)
+post '/questions/:id/comments' do
+    @question = Question.find(params[:id])
+    @comment = @question.comments.new(body:params[:content],user_id:current_user.id)
     if @comment.save
-    	@comment.question_id = @question.id
-    	@comment.save
       redirect "questions/#{@question.id}"
     else
-      @errors = post.errors.full_messages
+      @errors = @comment.errors.full_messages
       erb :'/comments/new'
     end
 end
 
-post '/comments/answer' do
-    @answer = Answer.find(params[:answer_id])
-    @comment = @answer.comments.new(content:params[:content],user_id:current_user.id)
+post '/answers/:id/comments' do
+    @answer = Answer.find(params[:id])
+    @comment = @answer.comments.new(body:params[:content],user_id:current_user.id)
     if @comment.save
-    	@comment.answer_id = @answer.id
-    	@comment.save
       redirect "questions/#{@answer.question_id}"
     else
-      @errors = post.errors.full_messages
+      @errors = @comment.errors.full_messages
       erb :'/comments/new'
     end
 end
