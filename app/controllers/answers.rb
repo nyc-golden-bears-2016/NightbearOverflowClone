8d529366cdbfd5f3766d3ec6b06c02b09cc3ad09
ericatalyst <erica825978@gmail.com>
2016-07-21 12:22:03 -0400

    Best answer working

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -29,12 +29,16 @@ end
 get '/answers/:id/bestanswer' do
   @answer = Answer.find(params[:id])
   @question = @answer.question
-  halt(401, "You do not have permission to complete this action.") unless logged_in? && current_user == @answer.user
+  halt(401, "You do not have permission to complete this action.") unless logged_in? && current_user == @question.user
   if @question.answers.include?(Answer.where(best_answer: true))
     @errors = ["Best answer already labeled"]
     redirect "/questions/#{@question.id}"
+  elsif @answer.user == current_user
+      @errors = ["Choosing your own answer??? Rude!"]
+      redirect "/questions/#{@question.id}"
   else
-    @answer.best_answer == true
+    @answer.update_attributes(best_answer: true)
+    @answer.save
     redirect "/questions/#{@question.id}"
   end
 end
