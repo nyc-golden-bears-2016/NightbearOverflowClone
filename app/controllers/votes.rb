17fd3142e54d248e2a721fdfccf3aeb472833527
marcomanomat <marco.manomat@gmail.com>
2016-07-21 16:37:08 -0400

    fix: current_user can only vote on a question once

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -3,9 +3,18 @@
 post '/:question_id/upvote' do
   @question = Question.find(params[:question_id])
   if logged_in?
-    vote = Vote.create(votable_id:params[:question_id],votable_type:"Question", user_id: current_user.id, value:1)
-    @question.total_votes += 1
-    @question.save
+    unless @question.votes.find_by(user_id: current_user.id)
+      if @question[:user_id] != current_user.id
+      vote = Vote.create(votable_id:params[:question_id],votable_type:"Question", user_id: current_user.id, value:1)
+      @question.total_votes += 1
+      @question.save
+      else
+        redirect "/questions/#{@question.id}"
+      end
+    else
+      @errors = ["You cannot vote more than once, loser."]
+      redirect "/questions/#{@question.id}"
+    end
   else 
     redirect '/sessions/login'
   end
@@ -15,9 +24,17 @@ end
 post '/:question_id/downvote' do
   @question = Question.find(params[:question_id])
   if logged_in?
-    vote = Vote.create(user_id: current_user.id, votable_id:params[:question_id], votable_type:"Question", value:-1)
-    @question.total_votes -= 1
-    @question.save
+    unless @question.votes.find_by(user_id: current_user.id)    
+      if @question[:user_id] != current_user.id
+      vote = Vote.create(user_id: current_user.id, votable_id:params[:question_id], votable_type:"Question", value:-1)
+      @question.total_votes -= 1
+      @question.save
+      else
+        redirect "/questions/#{@question.id}"
+      end    
+    else
+      redirect "/questions/#{@question.id}"
+    end
   end
   redirect "/questions/#{@question.id}"
 end
@@ -25,7 +42,7 @@ end
 post '/answers/:answer_id/upvote' do
   @answer=Answer.find(params[:answer_id])
   @question = @answer.question
-  if logged_in?
+  if logged_in? 
     vote = Vote.create(votable_id:@answer.id,votable_type:"Answer", user_id: current_user.id, value:1)
     @answer.total_votes += 1
     @answer.save    
@@ -36,10 +53,10 @@ end
 post '/answers/:answer_id/downvote' do
   @answer=Answer.find(params[:answer_id])
   @question = @answer.question
-  if logged_in?
+    if @answer[:user_id] != current_user.id        
     vote = Vote.create(user_id: current_user.id, votable_id:@answer.id, votable_type:"Answer", value:-1)
     @answer.total_votes -= 1
-    @answer.save      
+    @answer.save  
   end
   redirect "/questions/#{@question.id}"
 end
